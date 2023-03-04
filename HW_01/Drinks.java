3155a6da7820ce4811f75d3c3d360329a7b6e366
Alexey <lozhkin023@gmail.com>
2023-03-04 11:23:42 +0300

    add class drinks

---
+++ HW_01/Drinks.java
@@ -0,0 +1,15 @@
+package HW_01;
+
+public class Drinks extends Product {
+    /**
+     * Объем
+     */
+    protected Integer value;
+    public Drinks(String name, Integer price, Integer amount, String unit, Integer value) {
+        super(name, price, amount, unit);
+        this.value = value;
+    }
+    public Integer getValue() {
+        return value;
+    }
+}
