6fb74c10c1db0df63280ad45e2bacf68e74d8d6b
Alexey <lozhkin023@gmail.com>
2023-03-04 23:31:24 +0300

    add class paciefer

---
+++ HW_01/Pacifier.java
@@ -0,0 +1,9 @@
+package HW_01;
+
+public class Pacifier extends Children  {
+
+    public Pacifier(String name, Integer price, Integer amount, String unit, Integer minAge, String alergen) {
+        super(name, price, amount, unit, minAge, alergen);
+    }
+    
+}
