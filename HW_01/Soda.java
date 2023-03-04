678b23d7adf92b2eb1502361abe19f084298bbbd
Alexey <lozhkin023@gmail.com>
2023-03-04 23:31:37 +0300

    add class soda

---
+++ HW_01/Soda.java
@@ -0,0 +1,9 @@
+package HW_01;
+
+public class Soda extends Drinks{
+
+    public Soda(String name, Integer price, Integer amount, String unit, Integer value) {
+        super(name, price, amount, unit, value);
+    }
+    
+}
