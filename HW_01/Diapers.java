5cf879bfa12aad63b323834ec0787da7adeea1fa
Alexey <lozhkin023@gmail.com>
2023-03-04 23:30:38 +0300

    add class diapers

---
+++ HW_01/Diapers.java
@@ -0,0 +1,29 @@
+package HW_01;
+
+public class Diapers extends Children{
+    protected Integer size;
+    protected Integer minWeight;
+    protected Integer maxWeight;
+    protected String type;
+    public Diapers(String name, Integer price, Integer amount, String unit, Integer minAge, 
+    String alergen, Integer size,Integer minWeight, Integer maxWeight, String type) {
+        super(name, price, amount, unit, minAge, alergen);
+        this.maxWeight = maxWeight;
+        this.minWeight = minWeight;
+        this.size = size;
+        this.type = type;
+    }
+    public Integer getSize() {
+        return size;
+    }
+    public Integer getMinWeight() {
+        return minWeight;
+    }
+    
+    public Integer getMaxWeight() {
+        return maxWeight;
+    }
+    public String getType() {
+        return type;
+    }
+}
