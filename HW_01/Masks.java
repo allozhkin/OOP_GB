aaf7ac661b65aac6a2136551583a364c3bf554db
Alexey <lozhkin023@gmail.com>
2023-03-04 23:31:08 +0300

    add class masks

---
+++ HW_01/Masks.java
@@ -0,0 +1,9 @@
+package HW_01;
+
+public class Masks extends Hygiene {
+
+    public Masks(String name, Integer price, Integer amount, String unit, Integer pieces) {
+        super(name, price, amount, unit, pieces);
+    }
+    
+}
