a4d70277cbc0d17b5c788d6c1d89aa48f07654dd
Alexey <lozhkin023@gmail.com>
2023-03-04 11:23:02 +0300

    add class product

---
+++ HW_01/Product.java
@@ -0,0 +1,42 @@
+package HW_01;
+
+
+
+public class Product {
+    /**
+     * Наименование товара
+     */
+    protected String name;
+    /**
+     * Цена товара
+     */
+    protected Integer price;
+    /**
+     *  Количество товара
+     */
+    protected Integer amount;
+    /**
+     * Единица измерения
+     */
+    protected String unit;
+
+    public Product(String name,Integer price, Integer amount,String unit){
+        this.amount = amount;
+        this.name = name;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public Integer getPrice(){
+        return price;
+    }
+    public Integer getAmount(){
+        return amount;
+    }
+    public String getUnit(){
+        return unit;
+    }
+}
